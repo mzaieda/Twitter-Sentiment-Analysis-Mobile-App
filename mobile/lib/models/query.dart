ed7991ba6e2e7eb2cb0e76814be90ef7f5900815
DannyAntonelli <dannyantonelli2002@gmail.com>
2021-09-20 11:42:32 +0200

    Add methods to Query and Searches to manage JSON

--- mobile/lib/models/query.dart
+++ mobile/lib/models/query.dart
@@ -3,4 +3,14 @@ class Query {
   bool favorite;
 
   Query({required this.text, required this.favorite});
+
+  factory Query.fromJson(Map<String, dynamic> json) =>
+      Query(text: json['text'], favorite: json['favorite']);
+
+  Map<String, dynamic> toJsonEncodable() {
+    Map<String, dynamic> json = {};
+    json['text'] = text;
+    json['favorite'] = favorite;
+    return json;
+  }
 }
--- mobile/lib/models/searches.dart
+++ mobile/lib/models/searches.dart
@@ -3,4 +3,15 @@ import 'package:mobile/models/query.dart';
 class Searches {
   List<Query> recent = [];
   List<Query> favorites = [];
+
+  recentToJsonEncodable() =>
+      recent.map((query) => query.toJsonEncodable()).toList();
+
+  favoritesToJsonEncodable() =>
+      favorites.map((query) => query.toJsonEncodable()).toList();
+
+  void fromJson(List<dynamic> recent, List<dynamic> favorites) {
+    this.recent = recent.map((json) => Query.fromJson(json)).toList();
+    this.favorites = favorites.map((json) => Query.fromJson(json)).toList();
+  }
 }
