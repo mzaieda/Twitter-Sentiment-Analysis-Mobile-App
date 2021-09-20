dfaa494de71a250d16b6aea0f64176f603f9fb69
DannyAntonelli <dannyantonelli2002@gmail.com>
2021-09-20 11:40:25 +0200

    Refactor ChartData constructors

--- mobile/lib/models/chart_data.dart
+++ mobile/lib/models/chart_data.dart
@@ -7,30 +7,24 @@ class ChartData {
   final String label;
   final Color color;
 
-  factory ChartData.fromPositives(double positives) {
-    return ChartData(
-      "Positives",
-      positives,
-      "${(positives * 100).toStringAsFixed(2)}%",
-      Colors.green,
-    );
-  }
+  factory ChartData.fromPositives(double positives) => ChartData(
+        "Positives",
+        positives,
+        "${(positives * 100).toStringAsFixed(2)}%",
+        Colors.green,
+      );
 
-  factory ChartData.fromNeutrals(double neutrals) {
-    return ChartData(
-      "Neutrals",
-      neutrals,
-      "${(neutrals * 100).toStringAsFixed(2)}%",
-      Colors.grey,
-    );
-  }
+  factory ChartData.fromNeutrals(double neutrals) => ChartData(
+        "Neutrals",
+        neutrals,
+        "${(neutrals * 100).toStringAsFixed(2)}%",
+        Colors.grey,
+      );
 
-  factory ChartData.fromNegatives(double negatives) {
-    return ChartData(
-      "Negatives",
-      negatives,
-      "${(negatives * 100).toStringAsFixed(2)}%",
-      Colors.red,
-    );
-  }
+  factory ChartData.fromNegatives(double negatives) => ChartData(
+        "Negatives",
+        negatives,
+        "${(negatives * 100).toStringAsFixed(2)}%",
+        Colors.red,
+      );
 }
