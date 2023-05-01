295e5ad75cd7fd383f2475b83fb05d75643e0ce2
Mohammed Zaieda <m.zaieda@gmail.com>
2023-05-01 20:19:57 +0200

    first comment

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-
+import joblib
 
 
 def get_analysis(request, query):
@@ -7,23 +7,10 @@ def get_analysis(request, query):
         return JsonResponse({
             'error': 'GET request required'
         }, status=405)
-
-    # try:
-    #     NUM_OF_TWEETS = 50
-    #     client = TwitterClient()
-    #     analyzer = TweetsAnalyzer(client.get_tweets(query, NUM_OF_TWEETS))
-    # except tweepy.RateLimitError:
-    #     return JsonResponse({
-    #         'error': 'Twitter API rate limit exceeded.'
-    #     }, status=500)
-    # except tweepy.TweepError:
-    #     return JsonResponse({
-    #         'error': 'Error during the authentication with the Twitter API'
-    #     }, status=500)
     
     analysis = {'positives': 0.0,
-                'neutrals': 1.0,
-                'negatives': 0.0,
+                'neutrals': 0.5,
+                'negatives': 0.3,
                 'averageLikes': 0.0,
                 'averageRetweets': 0.0,}
     return JsonResponse(analysis, status=200)
